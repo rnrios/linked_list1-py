2dba782a525b40eab28ebbadfc6323d0528f43b0
Rodrigo <rodrigonr98@gmail.com>
2023-08-08 23:04:52 -0300

    insert and delete

--- linked_list.py
+++ linked_list.py
@@ -1,6 +1,43 @@
 class LinkedList:
-    def __int__(self):
-        self.head = None
+    def __init__(self, nodes=None):
+        node = Node(nodes.pop(0)) if nodes else None
+        self.head = node
+        while nodes:
+            node.next = Node(nodes.pop(0))
+            node = node.next
+
+    def add_first(self, node):
+        assert isinstance(node, Node)
+        node.next = self.head
+        self.head = node
+
+    def add_last(self, node_to_add):
+        assert isinstance(node_to_add, Node)
+        node = self.head
+        while node.next is not None:
+            node = node.next
+        node.next = node_to_add
+
+    def get_last(self):
+        node = self.head
+        while node.next is not None:
+            node = node.next
+        return node
+
+    def delete(self, value):
+        prev, node = None, self.head
+        while node.next is not None:
+            if node.data == value:
+                if prev is not None:
+                    prev.next = node.next
+                else:
+                    self.head = node.next
+                del node
+                print("Node deleted!")
+                return
+            prev = node
+            node = node.next
+        print("Node not found!")
 
     def __repr__(self):
         node = self.head
@@ -11,6 +48,12 @@ class LinkedList:
         nodes.append("None")
         return " -> ".join(nodes)
 
+    def __iter__(self):
+        node = self.head
+        while node is not None:
+            yield node
+            node = node.next
+
 
 class Node:
     def __init__(self, data):
@@ -19,3 +62,6 @@ class Node:
 
     def __repr__(self):
         return self.data
+
+    def __str__(self):
+        return str(self.data)
--- main.py
+++ main.py
@@ -1,17 +1,37 @@
 from linked_list import *
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    llist = LinkedList()
 
+if __name__ == '__main__':
+
+    print("First way of populating:")
+    llist = LinkedList()
     n1 = Node(1)
     llist.head = n1
-
     n2 = Node(2)
     n1.next = n2
-
     n3 = Node(3)
     n2.next = n3
+    print(llist)
+    print("=======================")
 
+    print("Second way of populating:")
+    llist = LinkedList([1, 2, 3])
     print(llist)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print("Traversing linked list:")
+    for i, node in enumerate(llist):
+        print(f'Node {i+1}: {node}')
+    print("=====================")
+
+    print("Adding element in 1st position:")
+    llist.add_first(Node(0))
+    print(f'First element: {llist.head}')
+    print("=====================")
+
+    print("Adding element in last position:")
+    llist.add_last(Node(4))
+    print(f'Last element: {llist.get_last()}')
+    print("=====================")
+
+    print("Deleting element")
+    llist.delete(0)
+    print(llist)
